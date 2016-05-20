78acf0df2b16f381d7363641a48e200b5e249a0c
Ronny Wang <ronnywang@gmail.com>
2016-05-20 10:43:21 +0800

    add Pix_Table_Db::addDbFromURI($uri)

--- Pix/Table/Db.php
+++ Pix/Table/Db.php
@@ -21,4 +21,41 @@ class Pix_Table_Db
 
         throw new Exception('不知道是哪類的 db' . get_class($obj));
     }
+
+    public static function addDbFromURI($uri)
+    {
+        if (strpos($uri, 'mysql://')) {
+            if (!preg_match('#mysql://([^:]*):([^@]*)@([^/]*)/(.*)#', strval($uri), $matches)) {
+                throw new Exception("wrong mysql URI format, must be mysql://{user}:{password}@{host}/{db}");
+            }
+
+            $db = new StdClass;
+            $db->host = $matches[3];
+            $db->username = $matches[1];
+            $db->password = $matches[2];
+            $db->dbname = $matches[4];
+            $config = new StdClass;
+            $config->master = $config->slave = $db;
+            Pix_Table::setDefaultDb(new Pix_Table_Db_Adapter_MysqlConf(array($config)));
+        } elseif (strpos($uri, 'pgsql://')) {
+            if (!preg_match('#pgsql://([^:]*):([^@]*)@([^/:]*):?([0-9]*)?/(.*)#', strval($uri), $matches)) {
+                throw new Exception("wrong pgsql URI format, must be pgsql://{user}:{password}@{host}/{db}");
+            }
+            $options = array(
+                'host' => $matches[3],
+                'user' => $matches[1],
+                'password' => $matches[2],
+                'dbname' => $matches[5],
+            );
+            if ($matches[4]) {
+                $options['port'] = $matches[4];
+            }
+            Pix_Table::setDefaultDb(new Pix_Table_Db_Adapter_PgSQL($options));
+        } elseif (strpos($uri, 'sqlite://')) {
+            $file = substr($uri, strlen('sqlite://'));
+            Pix_Table::setDefaultDb(new Pix_Table_Db_Adapter_Sqlite($file));
+        } else {
+            throw new Exception("add DB failed, unknown uri {$uri}");
+        }
+    }
 }
