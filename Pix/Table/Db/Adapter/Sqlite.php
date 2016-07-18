d8456e81638fff809fc2d097c806f2963ab43a77
Ronny Wang <ronnywang@gmail.com>
2016-07-18 17:31:05 +0800

    UNIQUE constraint failed on sqlite throw Pix_Table_DuplicateException

--- Pix/Table/Db/Adapter/Sqlite.php
+++ Pix/Table/Db/Adapter/Sqlite.php
@@ -43,7 +43,9 @@ class Pix_Table_Db_Adapter_Sqlite extends Pix_Table_Db_Adapter_SQL
                 $errorInfo = $this->_pdo->errorInfo();
             }
             if ($errorInfo[2] == 'PRIMARY KEY must be unique' or
-                    preg_match('/columns? .+ (are|is) not unique/', $errorInfo[2])) {
+                preg_match('/columns? .+ (are|is) not unique/', $errorInfo[2]) or
+                strpos($errorInfo[2], 'UNIQUE constraint failed:') === 0
+            ) {
                 throw new Pix_Table_DuplicateException();
             }
             throw new Exception("SQL Error: ({$errorInfo[0]}:{$errorInfo[1]}) {$errorInfo[2]} (SQL: {$sql})");
@@ -58,7 +60,9 @@ class Pix_Table_Db_Adapter_Sqlite extends Pix_Table_Db_Adapter_SQL
             $errorInfo = $statement->errorInfo();
         }
             if ($errorInfo[2] == 'PRIMARY KEY must be unique' or
-                    preg_match('/columns? .+ (are|is) not unique/', $errorInfo[2])) {
+                preg_match('/columns? .+ (are|is) not unique/', $errorInfo[2]) or
+                strpos($errorInfo[2], 'UNIQUE constraint failed:') === 0
+            ) {
                 throw new Pix_Table_DuplicateException();
             }
             throw new Exception("SQL Error: ({$errorInfo[0]}:{$errorInfo[1]}) {$errorInfo[2]} (SQL: {$sql})");
