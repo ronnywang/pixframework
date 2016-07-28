240fc9207725be8308f87709d5ba6a1c75cd9e58
Ronny Wang <ronnywang@gmail.com>
2016-07-28 14:24:28 +0800

    pgsql support geometry modifier

--- Pix/Table/Db/Adapter/PgSQL.php
+++ Pix/Table/Db/Adapter/PgSQL.php
@@ -120,7 +120,7 @@ class Pix_Table_Db_Adapter_PgSQL extends Pix_Table_Db_Adapter_SQL
                 $s .= 'INTEGER';
             } elseif ('binary' == $db_type) {
                 $s .= 'BYTEA';
-            } elseif ('geography' == $db_type) {
+            } elseif (in_array($db_type, array('geography', 'geometry'))) {
                 if (array_key_exists('modifier', $column)) {
                     $type = in_array(strtoupper($column['modifier'][0]), array('GEOMETRYCOLLECTION', 'POINT', 'LINESTRING', 'POLYGON', 'MULTIPOINT', 'MULTILINESTRING', 'MULTIPOLYGON')) ? strtoupper($column['modifier'][0]) : 'POINT';
                     $srid = array_key_exists(1, $column['modifier']) ? intval($column['modifier'][1]) : 0;
@@ -128,7 +128,7 @@ class Pix_Table_Db_Adapter_PgSQL extends Pix_Table_Db_Adapter_SQL
                     $type = 'POINT';
                     $srid = 0;
                 }
-                $s .= 'geography(' . $type . ',' . $srid . ')';
+                $s .= $db_type . '(' . $type . ',' . $srid . ')';
             } else {
                 $s .= strtoupper($db_type);
             }
